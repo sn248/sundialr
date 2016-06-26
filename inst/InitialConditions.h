db16241cf2fe3bda87b29592573be9a06f01fb79
sn248 <satyaprakash.nayak@hotmail.com>
2016-06-25 20:04:48 -0400

    writing the post - incomplete

---
+++ inst/InitialConditions.h
@@ -0,0 +1,6 @@
+#ifndef INITIALCONDITIONS_H
+#define INITIALCONDITIONS_H
+
+std::vector<double> InitialConditions();
+
+#endif    /* INITIALCONDITIONS_H */
--- src/sundials_test.cpp
+++ src/sundials_test.cpp
@@ -1,12 +1,28 @@
+// [[Rcpp::depends(BH)]]
 #include <Rcpp.h>
 
-// #include <iostream.h>
+#include <iostream>
+#include <vector>
+
 #include <cvode/cvode.h>               /* prototypes for CVODE fcts., consts. */
 #include <nvector/nvector_serial.h>    /* serial N_Vector types, fcts., macros */
 #include <cvode/cvode_dense.h>         /* prototype for CVDense */
 #include <sundials/sundials_dense.h>   /* definitions DlsMat DENSE_ELEM */
 #include <sundials/sundials_types.h>   /* definition of type realtype */
 
+using namespace Rcpp;
+
+void InitialConditions (NumericVector x){
+
+  N_Vector y = NULL;
+  // y = N_VNew_Serial(3);
+  //
+  // NV_Ith_S(y,0) = 1;
+  // NV_Ith_S(y,1) = 2;
+  // NV_Ith_S(y,2) = 3;
+
+
+}
 
 // [[Rcpp::export]]
 Rcpp::NumericVector timesTwo(Rcpp::NumericVector x) {
