ecc2838c5243eb944288681c7acb21afda1f7fde
sn248 <satyaprakash.nayak@hotmail.com>
2018-03-10 18:56:36 -0500

    Using the header file in /inst/include
    Based on PKG_CPPFILES - moving check_flag.h from /inst to /inst/include
    and removing the file checkflag.h from src

--- src/cvode.cpp
+++ src/cvode.cpp
@@ -9,7 +9,8 @@
 #include <sunmatrix/sunmatrix_dense.h>
 #include <sunlinsol/sunlinsol_dense.h>
 
-#include "checkflag.h"
+#include <check_flag.h>
+
 
 using namespace Rcpp;
 
@@ -181,4 +182,38 @@ NumericMatrix cvode (NumericVector time_vec, NumericVector IC, SEXP xpsexp,
 
   return soln;
   // return(0); // everything went fine
+}
+
+int check_flag(void *flagvalue, const char *funcname, int opt)
+{
+  int *errflag;
+
+  /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
+  if (opt == 0 && flagvalue == NULL)
+  {
+    // fprintf(stderr, "\n SUNDIALS_ERROR: %s() failed - returned NULL pointer \n\n", funcname);
+    Rprintf("SUNDIALS_ERROR: %s() failed - returned NULL pointer", funcname);
+    return(1);
+  }
+
+  /* Check if flag < 0 */
+  else if (opt == 1)
+  {
+    errflag = (int *) flagvalue;
+    if (*errflag < 0)
+    {
+      //fprintf(stderr, "\n SUNDIALS_ERROR: %s() failed with flag = %d \n\n", funcname, *errflag);
+      Rprintf("SUNDIALS_ERROR: %s() failed with flag = %d", funcname, *errflag);
+      return(1);
+    }
+  }
+  /* Check if function returned NULL pointer - no memory allocated */
+  else if (opt == 2 && flagvalue == NULL) {
+    // fprintf(stderr, "\n MEMORY_ERROR: %s() failed - returned NULL pointer\n\n", funcname);
+    Rprintf("MEMORY_ERROR: %s() failed - returned NULL pointer", funcname);
+    return (1);
+  }
+
+  return (0); // so returns 0 if everything is okay
+
 }
