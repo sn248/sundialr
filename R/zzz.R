0440eff0fb6e2549529e1d278efdd02e71343b51
sn248 <satyaprakash.nayak@hotmail.com>
2018-06-12 23:30:40 -0400

    Cleaned code, fixed soln and added an onAttach message
    Removed all the extraneous comments, solution is matching with example output and a message is displayed on attaching the package

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(...){
+  packageStartupMessage("SUNDIALS - Copyright (c) 2002-2015, Lawrence Livermore National Security.
+Produced at the Lawrence Livermore National Laboratory.
+See - https://computation.llnl.gov/projects/sundials")
+  }
