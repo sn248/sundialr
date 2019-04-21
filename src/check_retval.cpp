364ea548ed0817f3bdbb5fed58e7b7b96af183e4
sn248 <satyaprakash.nayak@hotmail.com>
2019-04-21 17:37:06 -0400

    cvode accepts parameters now
    Changed the interface of CVODE to accept parameters and check_flag is changed to check_retval to match SUNDIALS 4.1.0

---
+++ src/check_retval.cpp
@@ -0,0 +1,41 @@
+#include <Rcpp.h>
+
+// --- Function definition for check_flag() ------------------------------------
+/*
+ * Check function return value...
+ *   opt == 0 means SUNDIALS function allocates memory so check if
+ *            returned NULL pointer
+ *   opt == 1 means SUNDIALS function returns an integer value so check if
+ *            retval < 0
+ *   opt == 2 means function allocates memory so check if returned
+ *            NULL pointer
+ */
+
+int check_retval(void *returnvalue, const char *funcname, int opt)
+{
+  int *retval;
+
+  /* Check if SUNDIALS function returned NULL pointer - no memory allocated */
+  if (opt == 0 && returnvalue == NULL) {
+    Rprintf("\nSUNDIALS_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return(1); }
+
+  /* Check if retval < 0 */
+  else if (opt == 1) {
+    retval = (int *) returnvalue;
+    if (*retval < 0) {
+      Rprintf("\nSUNDIALS_ERROR: %s() failed with retval = %d\n\n",
+              funcname, *retval);
+      return(1); }}
+
+  /* Check if function returned NULL pointer - no memory allocated */
+  else if (opt == 2 && returnvalue == NULL) {
+    Rprintf("\nMEMORY_ERROR: %s() failed - returned NULL pointer\n\n",
+            funcname);
+    return(1); }
+
+  return(0);
+}
+
+// --- Function definition ends ------------------------------------------------
