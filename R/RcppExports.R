97f1c304d56503675495b125c84cab359ab59c72
sn248 <satyaprakash.nayak@hotmail.com>
2019-05-18 09:34:24 -0400

    CVODES works -- preliminary
    CVODES works, need to add checks and options in the code

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -15,3 +15,18 @@ cvode <- function(time_vector, IC, input_function, Parameters, reltolerance, abs
     .Call('_sundialr_cvode', PACKAGE = 'sundialr', time_vector, IC, input_function, Parameters, reltolerance, abstolerance)
 }
 
+#' cvodes
+#'
+#' CVODES solver to solve ODEs and calculate sensitivities
+#'@param time_vector time vector
+#'@param IC Initial Conditions
+#'@param input_function Right Hand Side function of ODEs
+#'@param Parameters Parameters input to ODEs
+#'@param reltolerance Relative Tolerance (a scalar)
+#'@param abstolerance Absolute Tolerance (a vector with length equal to ydot)
+#'@param SensType Sensitivity Type - allowed values are Staggered (default)", "Staggered1" or "Simulated"
+#'@param ErrCon Error Control - allowed values are TRUE or FALSE (default)
+cvodes <- function(time_vector, IC, input_function, Parameters, reltolerance, abstolerance, SensType = "STG", ErrCon = 'F') {
+    .Call('_sundialr_cvodes', PACKAGE = 'sundialr', time_vector, IC, input_function, Parameters, reltolerance, abstolerance, SensType, ErrCon)
+}
+
