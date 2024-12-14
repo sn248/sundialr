6b74358cbd96ac5ded10f19532413ad1c6b821b1
Satyaprakash Nayak <sn248@cornell.edu>
2024-12-14 11:56:49 -0500

    Remove downloaded sundials
    Download sundials source files during installation

--- src/scripts/sundials_download.sh
+++ src/scripts/sundials_download.sh
@@ -3,6 +3,6 @@
 RSCRIPT_BIN=$1
 
 ## Uncompress sundials source
-##${RSCRIPT_BIN} -e "download.file(url = 'https://github.com/LLNL/sundials/releases/download/v7.1.1/sundials-7.1.1.tar.gz', destfile = 'sundials-7.1.1.tar.gz')"
+${RSCRIPT_BIN} -e "download.file(url = 'https://github.com/LLNL/sundials/releases/download/v7.1.1/sundials-7.1.1.tar.gz', destfile = 'sundials-7.1.1.tar.gz')"
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'sundials-7.1.1.tar.gz')"
 mv sundials-7.1.1 sundials-src
