9e5f608005b02311c019da5fae1b4f29665f07d1
sn248 <sn248@cornell.edu>
2024-12-16 10:27:13 -0500

    Updates to incorporate issues
    See the changes suggested at https://github.com/sn248/sundialr/issues/9

--- src/scripts/sundials_download.sh
+++ src/scripts/sundials_download.sh
@@ -4,5 +4,11 @@ RSCRIPT_BIN=$1
 
 ## Uncompress sundials source
 ${RSCRIPT_BIN} -e "download.file(url = 'https://github.com/LLNL/sundials/releases/download/v7.2.0/sundials-7.2.0.tar.gz', destfile = 'sundials-7.2.0.tar.gz')"
+
+if [ $? -ne 0 ]; then
+    echo "Downloading sundials from github failed!"
+    exit 1
+fi
+
 ${RSCRIPT_BIN} -e "utils::untar(tarfile = 'sundials-7.2.0.tar.gz')"
 mv sundials-7.2.0 sundials-src
