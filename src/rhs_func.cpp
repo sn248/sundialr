eaac3203dcd11fdc579709bbae39ccbb09f3460b
sn248 <satyaprakash.nayak@hotmail.com>
2026-07-21 07:57:48 -0400

    Added UMBC as contributor. Fixed copyright dates throughout.

--- src/rhs_func.cpp
+++ src/rhs_func.cpp
@@ -1,4 +1,4 @@
-//   Copyright (c) 2024, Satyaprakash Nayak
+//   Copyright (c) 2016-2026, Satyaprakash Nayak
 //
 //   Redistribution and use in source and binary forms, with or without
 //   modification, are permitted provided that the following conditions are
